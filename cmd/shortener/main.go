1258f611907553b9b46bb6200ca9be272399c36e
Slava V <leterthing@gmail.com>
2023-05-03 01:23:22 +0700

    added flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	f "github.com/MrTomSawyer/url-shortener/cmd/shortener/config"
 	h "github.com/MrTomSawyer/url-shortener/cmd/shortener/httphandlers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	f.ParseFlags()
 	app := gin.Default()
 
 	app.POST("/", func(c *gin.Context) {
@@ -15,7 +17,7 @@ func main() {
 		h.GetOriginalURL(c.Writer, c.Request)
 	})
 
-	err := app.Run(`:8080`)
+	err := app.Run(f.ServerAddr)
 	if err != nil {
 		panic(err)
 	}
