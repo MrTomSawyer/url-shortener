202b9573d486381841361ec29037cabd7c0f9794
Slava V <leterthing@gmail.com>
2023-05-03 02:15:02 +0700

    revert default

--- cmd/shortener/tools/url.go
+++ cmd/shortener/tools/url.go
@@ -8,14 +8,9 @@ import (
 	f "github.com/MrTomSawyer/url-shortener/cmd/shortener/config"
 )
 
-var addr string = f.DefaultAddr
-
 func Shorten(url string) (string, string) {
 	hasher := md5.New()
 	hasher.Write([]byte(url))
 	hash := hex.EncodeToString(hasher.Sum(nil))[:8]
-	if addr == "" {
-		addr = "http://localhost:8080"
-	}
-	return fmt.Sprintf("%s/%s", addr, hash), hash
+	return fmt.Sprintf("%s/%s", f.DefaultAddr, hash), hash
 }
