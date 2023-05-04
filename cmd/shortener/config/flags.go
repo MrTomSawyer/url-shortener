664a9044936b05fd1ae225f7d223cd59b3662b2a
Slava V <leterthing@gmail.com>
2023-05-04 22:49:03 +0700

    returned flags

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -11,6 +11,7 @@ var DefaultAddr string
 func InitServerConfig() {
 	flag.StringVar(&ServerAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&DefaultAddr, "b", "http://localhost:8080", "default address and port of a shortened URL")
+	flag.Parse()
 
 	if envServerAddr := os.Getenv("SERVER_ADDRESS"); envServerAddr != "" {
 		ServerAddr = envServerAddr
