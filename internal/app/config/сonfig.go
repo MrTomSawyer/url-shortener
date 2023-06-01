e677cf4745155a405a64e34fad47d702a35eb1b6
Slava V <leterthing@gmail.com>
2023-06-01 23:21:22 +0700

    fix: env.Parse(a.Server)

--- internal/app/config/сonfig.go
+++ internal/app/config/сonfig.go
@@ -20,7 +20,7 @@ func (a *AppConfig) InitAppConfig() error {
 	flag.StringVar(&a.Server.TempFolder, "f", "/tmp/short-url-db.json", "default temp data storage path and filename")
 	flag.Parse()
 
-	err := env.Parse(a)
+	err := env.Parse(a.Server)
 	if err != nil {
 		return err
 	}
