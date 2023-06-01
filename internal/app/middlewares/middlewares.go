3202aab8aec3259ffadd4e4d24e2cafaf99c1cbc
Slava V <leterthing@gmail.com>
2023-06-01 23:45:25 +0700

    added package middlewares

--- internal/app/middlewares/middlewares.go
+++ internal/app/middlewares/middlewares.go
@@ -1,9 +1,7 @@
-package handler
+package middlewares
 
 import (
-	"compress/gzip"
 	"fmt"
-	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -11,7 +9,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Handler) logReqResInfo(lg logger) gin.HandlerFunc {
+type Logger interface {
+	Infof(template string, args ...interface{})
+}
+
+func LogReqResInfo(lg Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		startTime := time.Now()
 		c.Next()
@@ -21,7 +23,7 @@ func (h *Handler) logReqResInfo(lg logger) gin.HandlerFunc {
 	}
 }
 
-func (h *Handler) DataCompressor() gin.HandlerFunc {
+func DataCompressor() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		encoding := c.Request.Header.Get("Content-Encoding")
 
@@ -56,55 +58,5 @@ func (h *Handler) DataCompressor() gin.HandlerFunc {
 		} else {
 			c.Next()
 		}
-	}
-}
-
-type gzipBodyReader struct {
-	reader     io.ReadCloser
-	gzipReader *gzip.Reader
-}
-
-func (g *gzipBodyReader) Read(p []byte) (n int, err error) {
-	return g.gzipReader.Read(p)
-}
-
-func (g *gzipBodyReader) Close() error {
-	if err := g.reader.Close(); err != nil {
-		return err
-	}
-	if err := g.gzipReader.Close(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func newGzipBodyReader(body io.ReadCloser) (*gzipBodyReader, error) {
-	gzipReader, err := gzip.NewReader(body)
-	if err != nil {
-		return nil, err
-	}
-
-	return &gzipBodyReader{
-		reader:     body,
-		gzipReader: gzipReader,
-	}, nil
-}
-
-type gzipBodyWriter struct {
-	writer     http.ResponseWriter
-	gzipWriter *gzip.Writer
-}
-
-func (c *gzipBodyWriter) Close() error {
-	if err := c.gzipWriter.Close(); err != nil {
-		fmt.Printf("Failed to close gzip body writer: %v", err)
-	}
-	return nil
-}
-
-func newGzipBodyWriter(w http.ResponseWriter) *gzipBodyWriter {
-	return &gzipBodyWriter{
-		writer:     w,
-		gzipWriter: gzip.NewWriter(w),
 	}
 }
