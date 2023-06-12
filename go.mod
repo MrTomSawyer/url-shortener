e886d17f94928ddbf9496954dcc646b4a90d0f73
Slava V <leterthing@gmail.com>
2023-06-12 17:48:00 +0700

    reworking local tests

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.20
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/gin-gonic/gin v1.9.0
-	github.com/goccy/go-json v0.10.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9
 	go.uber.org/zap v1.24.0
 )
 
@@ -16,11 +17,11 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -35,6 +36,7 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/app/handler/shortenURL_test.go
+++ internal/app/handler/shortenURL_test.go
@@ -1,92 +1,93 @@
 package handler
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"strings"
-// 	"testing"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-// 	"github.com/MrTomSawyer/url-shortener/internal/app/config"
-// 	"github.com/MrTomSawyer/url-shortener/internal/app/logger"
-// 	"github.com/MrTomSawyer/url-shortener/internal/app/repository"
-// 	"github.com/MrTomSawyer/url-shortener/internal/app/service"
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/MrTomSawyer/url-shortener/internal/app/config"
+	"github.com/MrTomSawyer/url-shortener/internal/app/logger"
+	"github.com/MrTomSawyer/url-shortener/internal/app/repository"
+	"github.com/MrTomSawyer/url-shortener/internal/app/service"
+	"github.com/gin-gonic/gin"
+)
 
-// func TestShortenURL(t *testing.T) {
-// 	cfg := config.AppConfig{}
-// 	cfg.Server.DefaultAddr = "http://localhost:8080"
-// 	cfg.Server.ServerAddr = ":8080"
-// 	cfg.Server.TempFolder = ""
-// 	cfg.DataBase.ConnectionStr = ""
+func TestShortenURL(t *testing.T) {
+	cfg := config.AppConfig{}
+	cfg.Server.DefaultAddr = "http://localhost:8080"
+	cfg.Server.ServerAddr = ":8080"
+	cfg.Server.TempFolder = ""
+	cfg.DataBase.ConnectionStr = ""
 
-// 	err := logger.InitLogger()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	err := logger.InitLogger()
+	if err != nil {
+		panic(err)
+	}
 
-// 	type want struct {
-// 		code     int
-// 		response string
-// 	}
+	type want struct {
+		code     int
+		response string
+	}
 
-// 	tests := []struct {
-// 		name   string
-// 		url    string
-// 		method string
-// 		body   string
-// 		want   want
-// 	}{
-// 		{
-// 			name:   "Test #1 - Regular URL",
-// 			url:    "http://localhost:8080",
-// 			method: "POST",
-// 			body:   "https://yandex.ru",
-// 			want: want{
-// 				code:     201,
-// 				response: "http://localhost:8080/e9db20b2",
-// 			},
-// 		},
-// 		{
-// 			name:   "Test #2 - Empty Body",
-// 			url:    "http://localhost:8080",
-// 			method: "POST",
-// 			body:   "",
-// 			want: want{
-// 				code:     400,
-// 				response: "",
-// 			},
-// 		},
-// 	}
+	tests := []struct {
+		name   string
+		url    string
+		method string
+		body   string
+		want   want
+	}{
+		{
+			name:   "Test #1 - Regular URL",
+			url:    "http://localhost:8080",
+			method: "POST",
+			body:   "https://yandex.ru",
+			want: want{
+				code:     201,
+				response: "http://localhost:8080/e9db20b2",
+			},
+		},
+		{
+			name:   "Test #2 - Empty Body",
+			url:    "http://localhost:8080",
+			method: "POST",
+			body:   "",
+			want: want{
+				code:     400,
+				response: "",
+			},
+		},
+	}
 
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			w := httptest.NewRecorder()
-// 			с, _ := gin.CreateTestContext(w)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			с, _ := gin.CreateTestContext(w)
 
-// 			с.Request, _ = http.NewRequest(test.method, test.url, strings.NewReader(test.body))
+			с.Request, _ = http.NewRequest(test.method, test.url, strings.NewReader(test.body))
 
-// 			repo, err := repository.NewRepositoryContainer(cfg)
-// 			if err != nil {
-// 				fmt.Printf("Error creating repo container: %v", err)
-// 			}
-// 			serviceContainer, err := service.NewServiceContainer(repo, cfg)
-// 			if err != nil {
-// 				fmt.Printf("Error creating service container: %v", err)
-// 			}
-// 			h := Handler{
-// 				services: serviceContainer,
-// 			}
-// 			h.ShortenURL(с)
+			repo, err := repository.NewRepositoryContainer(context.Background(), cfg)
+			if err != nil {
+				fmt.Printf("Error creating repo container: %v", err)
+			}
+			serviceContainer, err := service.NewServiceContainer(repo, cfg)
+			if err != nil {
+				fmt.Printf("Error creating service container: %v", err)
+			}
+			h := Handler{
+				services: serviceContainer,
+			}
+			h.ShortenURL(с)
 
-// 			if с.Writer.Status() != test.want.code {
-// 				t.Errorf("got status code %d, want %d", w.Code, test.want.code)
-// 			}
+			if с.Writer.Status() != test.want.code {
+				t.Errorf("got status code %d, want %d", w.Code, test.want.code)
+			}
 
-// 			if body := strings.TrimSpace(w.Body.String()); body != test.want.response {
-// 				t.Errorf("got response body '%s', want '%s'", body, test.want.response)
-// 			}
-// 		})
-// 	}
-// }
+			if body := strings.TrimSpace(w.Body.String()); body != test.want.response {
+				t.Errorf("got response body '%s', want '%s'", body, test.want.response)
+			}
+		})
+	}
+}
