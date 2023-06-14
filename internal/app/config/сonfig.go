f4e29de7d18d7ee493f7cafee26a4e968a08db25
Slava V <leterthing@gmail.com>
2023-06-14 21:57:05 +0700

    commented psql connection string

--- internal/app/config/сonfig.go
+++ internal/app/config/сonfig.go
@@ -21,10 +21,10 @@ func (a *AppConfig) InitAppConfig() error {
 	flag.StringVar(&a.Server.ServerAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&a.Server.DefaultAddr, "b", "http://localhost:8080", "default address and port of a shortened URL")
 	flag.StringVar(&a.Server.TempFolder, "f", "/tmp/short-url-db.json", "default temp data storage path and filename")
-	// flag.StringVar(&a.DataBase.ConnectionStr, "d", "", "Database connection string")
+	flag.StringVar(&a.DataBase.ConnectionStr, "d", "", "Database connection string")
 
 	//для локальных тестов
-	flag.StringVar(&a.DataBase.ConnectionStr, "d", "host=localhost port=5432 user=myuser password=password dbname=mydb sslmode=disable", "Database connection string")
+	// flag.StringVar(&a.DataBase.ConnectionStr, "d", "host=localhost port=5432 user=myuser password=password dbname=mydb sslmode=disable", "Database connection string")
 	flag.Parse()
 
 	err := env.Parse(a)
