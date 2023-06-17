8afa9128f1f1989b7216970585e4054348aca020
Slava V <leterthing@gmail.com>
2023-06-17 23:36:36 +0700

    added logger to cookie middleware

--- internal/app/middlewares/cookie.go
+++ internal/app/middlewares/cookie.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/MrTomSawyer/url-shortener/internal/app/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
@@ -17,19 +18,23 @@ func CookieHandler(secret string) gin.HandlerFunc {
 		h := hmac.New(sha256.New, []byte(secret))
 
 		cookie, err := ctx.Cookie("user_id")
+		logger.Log.Info("Cookie: ", cookie, err)
 		if err != nil {
+			logger.Log.Info("Creating new cookie")
 			c := createUUIDCookie(h)
 			ctx.SetCookie("user_id", c, 0, "/", "", false, true)
 			ctx.Next()
 		}
 
 		if cookie == "" {
+			logger.Log.Info("Cookie is empry string")
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		values := strings.Split(cookie, "|")
 		if len(values) != 2 {
+			logger.Log.Info("Cookie includes less than 2 parts")
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
