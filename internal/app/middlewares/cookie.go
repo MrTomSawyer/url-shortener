d3dc46fdaf84a07b048eb7f01874652a91e295b2
Slava V <leterthing@gmail.com>
2023-06-18 19:05:45 +0700

    reworked cookie.go

--- internal/app/middlewares/cookie.go
+++ internal/app/middlewares/cookie.go
@@ -17,29 +17,22 @@ import (
 func CookieHandler(secret string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		h := hmac.New(sha256.New, []byte(secret))
-		logger.Log.Info("All cookies:", ctx.Request.Cookies())
-		cookie, err := ctx.Cookie("user_id")
-		logger.Log.Info("Cookie: %s", cookie)
 		maxAge := time.Now().Add(24 * time.Hour)
-		if err != nil {
-			logger.Log.Info("Cookie err: %s", err)
-			c, userID := createUUIDCookie(h)
-			logger.Log.Info("Creating userid for a cookie - ", userID)
 
+		cookie, err := ctx.Cookie("user_id")
+		if err != nil {
+			logger.Log.Info("No cookie found. Crearing a new one...")
+			c, userID := createUUIDCookie(h)
 			ctx.SetCookie("user_id", c, int(maxAge.Unix()), "/", "", false, true)
 			ctx.Set("user_id", userID)
 			ctx.Next()
-		}
-
-		if cookie == "" {
-			logger.Log.Info("Cookie is empry string")
-			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		values := strings.Split(cookie, "|")
+		logger.Log.Info("Cookie userid and signature: %v", values)
 		if len(values) != 2 {
-			logger.Log.Info("Cookie includes less than 2 parts")
+			logger.Log.Info("Invalid cookie format")
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
@@ -51,13 +44,14 @@ func CookieHandler(secret string) gin.HandlerFunc {
 		expectedSignature := hex.EncodeToString(h.Sum(nil))
 
 		if signature != expectedSignature {
-			logger.Log.Info("Cookie signature mismatch: %s", err)
+			logger.Log.Info("Invalid cookie signature. Crearing a new one...")
 			c, userID := createUUIDCookie(h)
-			logger.Log.Info("Creating userid for a cookie - ", userID)
 			ctx.SetCookie("user_id", c, int(maxAge.Unix()), "/", "", false, true)
 			ctx.Set("user_id", userID)
 			ctx.Next()
+			return
 		}
+
 		ctx.Set("user_id", userID)
 		ctx.Next()
 	}
